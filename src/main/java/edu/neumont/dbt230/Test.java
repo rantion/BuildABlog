ca90aff1368fc26a10d23b062fe729323399fff4
Jonathan Jensen <haiyun211@gmail.com>
2014-07-29 19:34:44 -0600

    Create CassandraInteract to interact with cassandra data

--- src/main/java/edu/neumont/dbt230/Test.java
+++ src/main/java/edu/neumont/dbt230/Test.java
@@ -10,19 +10,22 @@ import com.datastax.driver.core.Session;
  */
 public class Test {
     public static void main(String[] args) {
-        String serverHost = "137.135.57.176";
-        String keyspace = "dev";
+//        String serverHost = "137.135.57.176";
+//        String keyspace = "dev";
+//
+//        Cluster cluster = Cluster.builder().addContactPoints(serverHost).build();
+//
+//        Session session = cluster.connect(keyspace);
+//        String cqlStatement = "insert into emp (empid, emp_first, emp_last, emp_dept) values (2, 'willma', 'smith', 'eng')";
+//        session.execute(cqlStatement);
+//
+//        cqlStatement= "SELECT * FROM emp";
+//        ResultSet rs = session.execute(cqlStatement);
+//        for(Row row : rs) {
+//            System.out.println(row.toString());
+//        }
 
-        Cluster cluster = Cluster.builder().addContactPoints(serverHost).build();
-
-        Session session = cluster.connect(keyspace);
-        String cqlStatement = "insert into emp (empid, emp_first, emp_last, emp_dept) values (2, 'willma', 'smith', 'eng')";
-        session.execute(cqlStatement);
-
-        cqlStatement= "SELECT * FROM emp";
-        ResultSet rs = session.execute(cqlStatement);
-        for(Row row : rs) {
-            System.out.println(row.toString());
-        }
+        CassandraInteraction cassandra = new CassandraInteraction();
+        cassandra.printColumns();
     }
 }
